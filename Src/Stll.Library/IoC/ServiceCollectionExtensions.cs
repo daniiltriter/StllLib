dc19a32341965b62dc1e3dbcbfcfa131186b90d5
Daniil Gruzdev <daniilg@brokeree.com>
2024-01-09 09:34:39 +0300

    IoC extension for api added

---
+++ Src/Stll.Library/IoC/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Stll.Library.Abstractions;
+using Stll.Library.Implementation.Interfaces;
+using Stll.Library.Implementation.Services;
+using Stll.Library.Services;
+using Stll.Library.Settings;
+
+namespace Stll.Library.IoC;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection WithStllApi(this IServiceCollection services, Action<ApiSettings> settingsModifier)
+    {
+        services.AddOptions<ApiSettings>();
+        services.Configure(settingsModifier);
+        services.AddSingleton<IAuthenticationBridge, AuthenticationBridge>();
+
+        services.AddSingleton<IStllApiProvider, StllApiProvider>();
+        
+        return services;
+    }
+}
