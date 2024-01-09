e99cee4770221ed7dbf9b3c5f39a30c96daff77e
Daniil Gruzdev <daniilg@brokeree.com>
2024-01-09 08:56:51 +0300

    AthenticationApi interface added

---
+++ Src/Stll.Library/Internal/Api/Interfaces/IAuthenticationApi.cs
@@ -0,0 +1,10 @@
+﻿using Refit;
+using Stll.Library.Api.Objects;
+
+namespace Stll.Library.Api.Interfaces;
+
+public interface IAuthenticationApi
+{
+    [Post("api/oauth/token")]
+    Task<string> Token(JwtTokenApiRequest request);
+}
---
+++ Src/Stll.Library/Internal/Api/Objects/JwtTokenApiRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Stll.Library.Api.Objects;
+
+public class JwtTokenApiRequest
+{
+    public string Name { get; set; }
+    public string Password { get; set; }
+}
